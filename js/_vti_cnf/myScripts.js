0718ee8865f1f1fe301537ba9835536342a8b1eb
Ola <44835282+Ojebode@users.noreply.github.com>
2018-12-02 12:00:41 +0400

    Add files via upload

---
+++ js/_vti_cnf/myScripts.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|zenith-PC\\zenith
+vti_modifiedby:SR|zenith-PC\\zenith
+vti_timelastmodified:TR|26 May 2013 10:37:23 -0000
+vti_timecreated:TR|26 May 2013 08:55:04 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_nexttolasttimemodified:TW|26 May 2013 10:27:33 -0000
+vti_cacheddtm:TX|26 May 2013 10:27:33 -0000
+vti_filesize:IR|1209
---
+++ js/myScripts.js
@@ -0,0 +1,52 @@
+﻿	function validate()
+	{
+		var name = document.getElementById('firstname').value;
+		
+		if( name.length == 0 ) 
+		  { alert('You must enter a name');
+		    return false;
+		  }
+    
+         var lname = document.getElementById('lastname').value;
+		
+		if( lname.length == 0 ) 
+		  { alert('You must enter a name');
+		    return false;
+		  }
+
+         var contry = document.getElementById('country ').value;
+		
+		if( contry.length == 0 ) 
+		  { alert('You must enter your present country');
+		    return false;
+		  }
+		var mail = document.getElementById('email ').value;
+		
+		if( mail .length == 0 ) 
+		  { alert('You must enter your Email ');
+		    return false;
+		  }
+		  
+		var add = document.getElementById('address').value;
+		
+		if( add .length == 0 ) 
+		  { 
+		  alert('You must enter address');
+		    return false;
+		  }  
+		
+			  
+         var mob = document.getElementById('mobileno').value;
+		
+		if( mob.length == 0 ) 
+		  { alert('You must enter yor mobile number');
+		    return false;
+		  }
+
+		  
+		document.getElementById('loginForm').submit();
+		return true;
+	}
+	
+	document.getElementById('submitButton').addEventListener('click',validate);
+		
